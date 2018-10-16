728627ae53310a490917cb07cb67e218b4d0ee36
f.p.khodaparast <f.khodaparast@mail.sbu.ac.ir>
2018-10-16 23:42:16 +0330

    Add files via upload

---
+++ tree/Constant.java
@@ -0,0 +1,35 @@
+package lda;
+
+/**
+ * Created by "P.Khodaparast" on 2018-09-20.
+ */
+public final class Constant {
+    public  static final String MEAN_SENDER_TO_COORDINATOR_YES="p5.1";
+    public  static final String MEAN_SENDER_TO_COORDINATOR_NO="p5.2";
+    public  static final String COV_SENDER_TO_COORDINATOR_YES_INT="p5.3";
+    public  static final String COV_SENDER_TO_COORDINATOR_YES_DECI="p5.4";
+    public  static final String COV_SENDER_TO_COORDINATOR_NO_INT="p5.5";
+    public  static final String COV_SENDER_TO_COORDINATOR_NO_DECI="p5.6";
+
+
+    public  static final String FILE_PATH_PARTY_1="src/lda/LDA_HE_tree/p1/d_1000_000.csv";
+    public  static final String FILE_PATH_PARTY_2="src/lda/LDA_HE_tree/p2/d_1000_000.csv";
+    public  static final String FILE_PATH_PARTY_3="src/lda/LDA_HE_tree/p3/d_1000_000.csv";
+    public  static final String FILE_PATH_PARTY_4="src/lda/LDA_HE_tree/p4/d_1000_000.csv";
+    public  static final String FILE_PATH_PARTY_5="src/lda/LDA_HE_tree/p5/d_1000_000.csv";
+
+    public  static final String TIME_COMPLEXITY_PATH= "src/resources/timeComplexity";
+    public  static final String FACTORY_NAME="java:comp/DefaultJMSConnectionFactory";
+    public  static final String PARTY_1_INPUT_Q="Queue01";
+    public  static final String PARTY_1_OUTPUT_Q="Queue03";
+    public  static final String PARTY_2_INPUT_Q="Queue02";
+    public  static final String PARTY_2_OUTPUT_Q="Queue04";
+    public  static final String PARTY_3_INPUT_Q="Queue03";
+    public  static final String PARTY_3_OUTPUT_Q="Queue05";
+
+    public  static final String PARTY_4_INPUT_Q="Queue04";
+    public  static final String PARTY_4_OUTPUT_Q="Queue05";
+    public  static final String PARTY_5_INPUT_Q="Queue05";
+    public  static final String PARTY_5_OUTPUT_Q="Queue06";
+
+}
